5a1cf3562f1121b0e5403195efd0c053e366e9ff
league <league@imac-28.attlocal.net>
2023-12-16 11:20:19 -0800

    eddx

--- src/_00_intro_to_array_lists/IntroToArrayLists.java
+++ src/_00_intro_to_array_lists/IntroToArrayLists.java
@@ -1,20 +1,41 @@
 package _00_intro_to_array_lists;
 
+import java.util.ArrayList;
+
 public class IntroToArrayLists {
 	public static void main(String[] args) {
 		//1. Create an array list of Strings
 		//   Don't forget to import the ArrayList (java.util) class
-
+		ArrayList<String> names = new ArrayList<String>();
 		//2. Add five Strings to your list
-		
+		names.add("Jan");
+		names.add("hyunj6682@my.sduhsd.net ");
+		names.add("Fred");
+		names.add("Marc");
+		names.add("jeonghoo");
 		//3. Print all the Strings using a standard for-loop
-		
+		for(int i = 0; i < names.size(); i++){
+			String s = names.get(i);
+			System.out.println("String at element " + i + " " + s);
+		}
 		//4. Print all the Strings using a for-each loop
-		
+		for(String s : names){
+			System.out.println(s);
+		}
 		//5. Print only the even numbered elements in the list.
-		
+		System.out.println(names.get(2));
+		System.out.println(names.get(4));
 		//6. Print all the Strings in reverse order.
-		
+		for(int i = 4; i >= 0; i--){
+			System.out.println(names.get(i));
+		}
 		//7. Print only the Strings that have the letter 'e' in them.
+	
+		for(int i = 0; i < names.size(); i++){
+			if(names.get(i).contains("e")) {
+				System.out.println(names.get(i));
+			
+			}
+		}
 	}
 }
--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -16,21 +16,32 @@ public class VisualArrayLists implements ActionListener {
 
 	public void addThingsToList() {
 		// 1. Add a bunch of items to our list (4 or 5 should be enough!)
+		list.add("e");
+		list.add("g");
+		list.add("x");
+		list.add("l");
 		
 		// 2. Set one item in our list to be something different
-		
+		list.set(1,"y");
 		// 3. Remove something from our list
-		
+		list.remove(2);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		for(int i = 0; i < list.size(); i++){
+			if(list.get(i).contains("e")) {
+				System.out.println(list.get(i));
+			
+			}
+			
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(1));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
